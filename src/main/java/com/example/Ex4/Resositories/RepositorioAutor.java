3651408754572a812ff5a7126e50a6a38de6be00
Sushhi-cdr <enzoseiti@gmail.com>
2026-03-17 15:27:56 -0300

    Criação do repositories

---
+++ src/main/java/com/example/Ex4/Resositories/RepositorioAutor.java
@@ -0,0 +1,7 @@
+package com.example.Ex4.Resositories;
+
+import com.example.Ex4.Models.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioAutor extends JpaRepository<Autor, Long> {
+}
